f14fa2326ed28ecb35680d68b3f52b7858f9c076
Marissa Zhong <mwz2101@columbia.edu>
2018-06-28 01:57:03 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,9 +2,9 @@
 class Dog
 
   @@all = []
-  
+
   attr_accessor :name
-  
+
   def initialize(name)
     @name = name
     @@all << self
@@ -22,4 +22,4 @@ class Dog
     @@all = @@all.clear
   end
 
-endß
+end
